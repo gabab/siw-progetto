e89a133457f856c2bb89071f9ecce1fa37ea78c3
irene <iresbriss@gmail.com>
2015-05-29 15:33:29 +0200

    modificato un metodo della classe CostomerHandler

--- src/it/uniroma3/controller/CustomerHandler.java
+++ src/it/uniroma3/controller/CustomerHandler.java
@@ -1,6 +1,5 @@
 package it.uniroma3.controller;
 
-
 import it.uniroma3.facades.OrderFacade;
 import it.uniroma3.facades.ProductFacade;
 import it.uniroma3.facades.UserFacade;
@@ -20,76 +19,71 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class CustomerHandler {
 
-    //@ManagedProperty(value = "#{login.getCustomer}")
-    private Customer currentCustomer;
-    private List<Order> orders;
-    private Order order;
-    private Order currentOrder;
-    @EJB(name = "order")
-    private OrderFacade orderFacade;
-    @EJB(name = "product")
-    private ProductFacade productFacade;
-    @EJB(name = "user")
-    private UserFacade userFacade;
-    
-    public void createOrder(){
-    	this.currentOrder = new Order(currentCustomer);
-    }
+	// @ManagedProperty(value = "#{login.getCustomer}")
+	private Customer currentCustomer;
+	private List<Order> orders;
+	private Order order;
+	private Order currentOrder;
+	@EJB(name = "order")
+	private OrderFacade orderFacade;
+	@EJB(name = "product")
+	private ProductFacade productFacade;
+	@EJB(name = "user")
+	private UserFacade userFacade;
 
-    public void addProductToOrder(Long productID, int quantity){
-    	Product p = this.productFacade.getProduct(productID);
-    	this.currentOrder.addProductToOrder(p, quantity);
-    }
-    
-    public OrderFacade getOrderFacade() {
-        return orderFacade;
-    }
-    
-    public String closeOrder() {
-    	this.currentOrder.setState(OrderState.CLOSED);
-    	this.currentOrder.setClosed(new Date());
-    	this.currentCustomer.addOrder(this.currentOrder);
-       	this.userFacade.updateUser(this.currentCustomer);// a cascata una volta che aggiorna il customer aggiorna tutto
-    	return "confirmation";
-    }
+	public OrderFacade getOrderFacade() {
+		return orderFacade;
+	}
 
-    public void setOrderFacade(OrderFacade orderFacade) {
-        this.orderFacade = orderFacade;
-    }
+	public String closeOrder() {
+		this.currentOrder.setState(OrderState.CLOSED);
+		this.currentOrder.setClosed(new Date());
+		this.currentCustomer.addOrder(this.currentOrder);
+		this.userFacade.updateUser(this.currentCustomer);// a cascata una volta
+															// che aggiorna il
+															// customer aggiorna
+															// tutto
+		return "confirmation";
+	}
 
-    public Customer getCurrentCustomer() {
-        return currentCustomer;
-    }
+	public void setOrderFacade(OrderFacade orderFacade) {
+		this.orderFacade = orderFacade;
+	}
 
-    public void setCurrentCustomer(Customer currentCustomer) {
-        this.currentCustomer = currentCustomer;
-    }
+	public Customer getCurrentCustomer() {
+		return currentCustomer;
+	}
 
-    public Order getOrder() {
-        return order;
-    }
+	public void setCurrentCustomer(Customer currentCustomer) {
+		this.currentCustomer = currentCustomer;
+	}
 
-    public void setOrder(Order order) {
-        this.order = order;
-    }
+	public Order getOrder() {
+		return order;
+	}
 
-    public String getOrders() {
-        this.orders = this.currentCustomer.getOrders();
-        return "myOrders";
-    }
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
+	public String getOrders() {
+		this.orders = this.currentCustomer.getOrders();
+		return "myOrders";
+	}
 
-    public String findOrder(Long orderID) {
-        this.order = this.orderFacade.getOrder(orderID);
-        return "orderDetail";
-    }
-    
-    public void addProductToCart(Long productID, int quantity) {
-    	if (currentOrder == null)
-    		this.createOrder();
-    	this.addProductToOrder(productID, quantity);
-    }
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+
+	public String findOrder(Long orderID) {
+		this.order = this.orderFacade.getOrder(orderID);
+		return "orderDetail";
+	}
+
+	public void addProductToCart(Long productID, int quantity) {
+		if (currentOrder == null)
+			this.currentOrder = new Order(currentCustomer);
+		Product p = this.productFacade.getProduct(productID);
+		this.currentOrder.addProductToOrder(p, quantity);
+	}
 }
